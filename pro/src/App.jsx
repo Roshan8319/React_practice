abe5051ddeb9f7ba7a89d169bb6df76104a3f87f
Roshan Prasad Sahu <roshanprasad447@gmail.com>
2024-07-25 11:35:05 +0530

    ad

---
+++ pro/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [color, setColor] = useState("olive")
+
+  return (
+    <>
+      <div className="w-screen h-4 bg-red-600">
+       </div>
+    </>
+  )
+}
+
+export default App
